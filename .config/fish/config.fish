ee590fd06d786e88741768e10c7649fa114d6c66
leofleeo <83542885+leofleeo@users.noreply.github.com>
2026-02-01 14:02:09 -0500

    Add starship prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,12 +5,14 @@ end
 fish_config theme choose "Catppuccin Mocha"
 
 function fish_greeting
-    echo "Hi twin welcome to the terminal"
+
 end
 
 # pnpm
-set -gx PNPM_HOME "/home/devcat/.local/share/pnpm"
+set -gx PNPM_HOME "~/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
     set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+starship init fish | source
